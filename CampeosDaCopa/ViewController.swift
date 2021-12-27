f3be24680f1bf9afaf5d7b40dcd257e7a0840e83
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2021-12-27 15:23:28 -0300

    project finally

---
+++ CampeosDaCopa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampeosDaCopa
+//
+//  Created by Victor Feitosa on 27/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
